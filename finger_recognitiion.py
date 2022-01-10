24c08d4eeb3ca9f8503d11ee622e25a62b3365ea
mapletea <peggy471665@gmail.com>
2022-01-10 12:15:08 +0800

    opt recognize fingers

--- finger_recognitiion.py
+++ finger_recognitiion.py
@@ -2,11 +2,24 @@ import pandas as pd
 import numpy as np
 import csv
 
+def finger_stand(points):
+    fingers = []
+    # if (points['y'][4] < points['y'][2]): # 拇指
+    #     fingers.append(0)
+    if (points['y'][8] < points['y'][6]): # 食指
+        fingers.append(1)
+    if (points['y'][12] < points['y'][10]): # 中指
+        fingers.append(2)
+    if (points['y'][16] < points['y'][14]): # 無名指
+        fingers.append(3)
+    if (points['y'][20] < points['y'][18]): # 小指
+        fingers.append(4)
+    return fingers
+
 def ishid0(points):
     x_diff = max(abs(points['x'][5] - points['x'][9]), abs(points['x'][6] - points['x'][10]), abs(points['x'][7] - points['x'][11]), abs(points['x'][8] - points['x'][12]))
-    up_mean = np.mean(points.loc[5:13, 'y'].values) 
-    down_mean = np.mean(points.loc[:5, 'y'].values)+np.mean(points.loc[13:, 'y'].values)/2
-    if (x_diff < 50) & (up_mean < down_mean):
+    fingers = finger_stand(points)
+    if (fingers == [1, 2]) & (x_diff < 50):
         return True
     else:
         return False
